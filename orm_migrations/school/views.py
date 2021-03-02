9ae44d535b90d400e5839348b2771f9db3590b5f
Karina <ka_ri_n_ka@mail.ru>
2021-03-02 23:10:53 +0300

    Optimized views and students_list

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -5,15 +5,7 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    list_students = []
     students = Student.objects.all().prefetch_related('teachers').order_by('group')
-    for student in students:
-        object_student = {}
-        object_student['name'] = student.name
-        object_student['group'] = student.group
-        object_student['teachers'] = student.teachers.all().order_by('name').values('name', 'subject')
-
-        list_students.append(object_student)
-    context = {'list_students': list_students}
+    context = {'list_students': students}
 
     return render(request, template, context)
